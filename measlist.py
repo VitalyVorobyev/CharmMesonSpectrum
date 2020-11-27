2a2da6eba3375128b8e9af6b25b5857f4ec06555
Vitaly Vorobyev <vit.vorobiev@gmail.com>
2020-11-27 14:57:32 +0700

    M178, 179, 129

--- measlist.py
+++ measlist.py
@@ -1,26 +1,43 @@
 
-MEAS = [
-    {
-        'pub': 'LINK 04A',
-        'charge': 0,
-        'type': 'incl',
-        'finalState': r'$D^+\pi^-$',
-        'JCP': None,
-        'mass': {
-            'value': 2407,
-            'stat': 21,
-            'syst': 35,
-        },
-        'width': {
-            'value': 240,
-            'stat': 55,
-            'syst': 59,
-        },
-        'process': r'$A\gamma$',
-        'pdg': 'M178',
-    },
-    {
-        
+def mval(val, stat, syst):
+    return {
+        'value': val,
+        'stat': stat,
+        'syst': syst,
     }
+
+def meas_item(pub, charge, incl, fState, jp, mass, width, process, yld, pdg, used):
+    return {
+        'pub': pub,
+        'charge': charge,
+        'incl': incl,
+        'finalState': fState,
+        'JP': jp,
+        'mass': mass,
+        'width': width,
+        'process': process,
+        'yield': yld,
+        'pdg': pdg,
+        'used': used,
+    }
+
+MEAS = [
+    meas_item(*args) for args in [
+        #   pub           q  incl      finalState        JP     mass                width                     process                         yield   PDG    used
+        ['LINK 04A'     , 0, True,  r'$D^+\pi^-$',         None, mval(2407, 21, 35), mval(240, 55, 59),     r'$A\gamma$'                    ,  None, 'M178', False], # M179
+        ['ABE 04D'      , 0, False, r'$D^+\pi^-$',     r'$0^+$', mval(2308, 17, 32), mval(240, 55, 59),     r'$B^-\to D^+\pi^-\pi^+$'       , 1.1e3, 'M178', True],
+        ['AUBERT 09AB'  , 0, False, r'$D^+\pi^-$',     r'$0^+$', mval(2297,  8, 20), mval(273, 12, 48),     r'$B^-\to D^+\pi^-\pi^+$'       , 3.4e3, 'M178', True],  # M178
+        ['LINK 04A'     , 1, True,  r'$D^0\pi^-$',         None, mval(2403, 14, 35), mval(283, 24, 34),     r'$A\gamma$'                    ,  None, 'M179', False],
+        ['AAIJ 15Y'     , 1, False, r'$D^0\pi^-$',     r'$0^+$', mval(2349,  6,  4), mval(217, 13, 13),     r'$B^0\to \bar{D}^0\pi^-\pi^+$' , 9.6e3, 'M179', True],
+        ['AAIJ 15X'     , 1, False, r'$D^0\pi^-$',     r'$0^+$', mval(2360, 15, 30), mval(255, 26, 51),     r'$B^0\to \bar{D}^0\pi^-K^+$'   , 2.6e3, 'M179', True],
+        ['ANJOS 89C'    , 1, True,  r'$D^{*0}\pi^+$',      None, mval(2443,  7,  5), mval( 49, 19,  8),     r'$\gamma N\to D^{*0}\pi^+X^0$' ,   190, 'M120', True],  # M120
+        ['BERGFELD 94B' , 1, True,  r'$D^{*0}\pi^+$',      None, mval(2425,  2,  2), mval( 26, [7, 8],  4), r'$e^+e^-\to D^{*0}\pi^+X$'     ,   146, 'M120', True],
+        ['ABE 05A'      , 1, False, r'$D^+\pi^+\pi^-$',    None, mval(2421,  2,  1), mval( 21,  5,  8),     r'$\bar{B}^0\to D^+\pi^+2\pi^-$',   124, 'M120', True],
+        ['ABRAMOWICZ 13', 1, True,  r'$D^{*0}\pi^-$',      None, mval(2421.9, 4.7, [1.2, 3.4]), None,       r'$e^{\pm}p\to D^{(*)0}\pi^+X$' ,   759, 'M120', True],
+    ]
 ]
 
+NOTES = [
+    """ M179: remove 'J, P need confirmation' from header """,
+    """ M120: ANJOS 89C comment: "D0 pi+" -> "D*0 pi+" """,
+]
--- publist.py
+++ publist.py
@@ -1,22 +1,49 @@
+def pub_item(doi, ref, year, exp):
+    return {'doi': doi, 'ref': ref, 'year': year, 'exp': exp}
 
 PUBS = {
-    'LINK 04A' : {
-        'doi': 'https://doi.org/10.1016/j.physletb.2004.02.017',
-        'ref': 'Phys. Lett. B586 (2004) 11',
-        'year': 2004,
-        'exp': 'FOCUS',
-    },
-    'ABE 04D' : {
-        'doi': 'https://doi.org/10.1103/PhysRevD.69.112002',
-        'ref': 'Phys. Rev. D 69 (2004) 112002',
-        'year': 2004,
-        'exp': 'Belle',
-    },
-    'AUBERT 09AB' : {
-        'doi': 'https://doi.org/10.1103/PhysRevD.79.112004',
-        'ref': 'Phys. Rev. D 79 (2009) 112004',
-        'year': 2009,
-        'exp': 'BaBar',
-    },
-    
+    'LINK 04A' : pub_item(
+        'https://doi.org/10.1016/j.physletb.2004.02.017',
+        'Phys. Lett. B586 (2004) 11', 2004, 'FOCUS',
+    ),
+    'ABE 04D' : pub_item(
+        'https://doi.org/10.1103/PhysRevD.69.112002',
+        'Phys. Rev. D 69 (2004) 112002', 2004, 'Belle',
+    ),
+    'AUBERT 09AB' : pub_item(
+        'https://doi.org/10.1103/PhysRevD.79.112004',
+        'Phys. Rev. D 79 (2009) 112004', 2009, 'BaBar',
+    ),
+    'AAIJ 15Y' : pub_item(
+        'https://doi.org/10.1103/PhysRevD.92.032002',
+        'Phys. Rev. D 92 (2015) 032002', 2015, 'LHCb'
+    ),
+    'AAIJ 15X' : pub_item(
+        'https://doi.org/10.1103/PhysRevD.92.012012',
+        'Phys. Rev. D 92 (2015) 012012', 2015, 'LHCb'
+    ),
+    'ANJOS 89C': pub_item(
+        'https://doi.org/10.1103/PhysRevLett.62.1717',
+        'Phys. Rev. Lett 62 (1989) 1717', 1989, 'E691'
+    ),
+    'BERGFELD 94B': pub_item(
+        'https://doi.org/10.1016/0370-2693(94)01348-9',
+        'Phys. Lett. B 331 (1994) 236', 1994, 'CLEO'
+    ),
+    'ABE 05A': pub_item(
+        'https://doi.org/10.1103/PhysRevLett.94.221805',
+        'Phys. Rev. Lett. 94 (2005) 221805', 2005, 'Belle'
+    ),
+    'AUBERT 09Y': pub_item(
+        'https://doi.org/10.1103/PhysRevLett.103.051803',
+        'Phys. Rev. Lett. 103 (2009) 051803', 2009, 'BaBar'
+    ),
+    'ABRAMOWICZ 13': pub_item(
+        'https://doi.org/10.1016/j.nuclphysb.2012.09.007',
+        'Nucl. Phys. B. 866 (2013) 229', 2013, 'ZEUS'
+    ),
+    'ABLIKIM 20P': pub_item(
+        'https://doi.org/10.1016/j.physletb.2020.135395',
+        'Phys. Lett. B 804 (2020) 135395', 2020, 'BESIII'
+    ),
 }
