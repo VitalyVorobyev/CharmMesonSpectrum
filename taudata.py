1853a499ac8018b43be4bc4aab79d99037f7162f
Vitaly Vorobyev <vit.vorobiev@gmail.com>
2021-12-22 21:30:57 +0700

    tau data wip

---
+++ taudata.py
@@ -0,0 +1,16 @@
+
+tau_lifetime = [
+    [(290.17, 0.53, 0.33), 'Belle:2013teo'],
+    [(290.9, 1.4, 1.0), 'DELPHI:2003zcz'],
+    [(293.2, 2.0, 1.5), 'L3:2000dya'],
+    [(290.1, 1.5, 1.1), 'ALEPH:1997roz'],
+    [(289.2, 1.7, 1.2), 'OPAL:1996dap'],
+    [(289.0, 2.8, 4.0), 'CLEO:1996hqs']
+]
+
+tau_lifetime_pdg = (290.3, 0.5)
+tau_lifetime_diff = 7.0e-3  # (t+ - t-) / t0, Belle:2013teo
+
+tau_mass = [
+
+]
---
+++ tauplot.py
@@ -0,0 +1,9 @@
+from taudata import tau_lifetime, tau_lifetime_pdg
+
+import numpy as np
+import matplotlib.pyplot as plt
+import matplotlib
+matplotlib.rcParams['font.size'] = 16
+
+def tau_lifetime_plot():
+    pass
