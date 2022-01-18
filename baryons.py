93b4b5cba8910cbbde8dce183e95cb89ed7e226c
Vitaly Vorobyev <vit.vorobiev@gmail.com>
2022-01-18 19:39:08 +0700

    baryons init

---
+++ baryons.py
@@ -0,0 +1,20 @@
+STATES = {
+    'lamc': {
+        'name': r'$\Lambda_c^+$',
+        'jp': r'$1/2^+$',
+        'massPDG': (2286.46, 0.14),
+        'ctauPDG': (202.4, 3.1),  # fs
+    },
+    'lamc2595': {
+        'name': r'$\Lambda_c(2595)^+$',
+        'jp': r'$1/2^+$',
+        'massPDG': (2592.25, 0.28),
+        'widthPDG': (2.6, 0.6),  # MeV
+    },
+    'lamc2625': {
+        'name': r'$\Lambda_c(2595)^+$',
+        'jp': r'$1/2^+$',
+        'massPDG': (2628.11, 0.19),
+        'widthPDG': (2.6, 0.6),  # MeV
+    }
+}
